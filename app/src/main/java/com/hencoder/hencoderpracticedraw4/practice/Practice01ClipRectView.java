daa359471168f7ad1346dbba24c1b26f16a9edd0
huangqiao <huangqiao92@gmail.com>
2018-08-13 14:38:27 +0800

    finish.

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
@@ -8,36 +8,38 @@ import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
 import com.hencoder.hencoderpracticedraw4.R;
 
 public class Practice01ClipRectView extends View {
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Bitmap bitmap;
+  Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+  Bitmap bitmap;
 
-    public Practice01ClipRectView(Context context) {
-        super(context);
-    }
+  public Practice01ClipRectView(Context context) {
+    super(context);
+  }
 
-    public Practice01ClipRectView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  public Practice01ClipRectView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
 
-    public Practice01ClipRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  public Practice01ClipRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
 
-    {
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
-    }
+  {
+    bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+  }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
 
-        int left = (getWidth() - bitmap.getWidth()) / 2;
-        int top = (getHeight() - bitmap.getHeight()) / 2;
+    int left = (getWidth() - bitmap.getWidth()) / 2;
+    int top = (getHeight() - bitmap.getHeight()) / 2;
 
-        canvas.drawBitmap(bitmap, left, top, paint);
-    }
+    canvas.save();
+    canvas.clipRect(left + 50, top + 100, left + 450, top + 200);
+    canvas.drawBitmap(bitmap, left, top, paint);
+    canvas.restore();
+  }
 }
